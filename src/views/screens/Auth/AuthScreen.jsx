a48e2e106495de87ee922c86f83a909bbee9ab9f
ernandarn14 <ernanda.rully@gmail.com>
2020-04-27 14:22:08 +0700

    add register form

--- src/views/screens/Auth/AuthScreen.jsx
+++ src/views/screens/Auth/AuthScreen.jsx
@@ -6,6 +6,10 @@ class AuthScreen extends React.Component {
     render() {
         return (
             <div className="container">
+                <div className="row mt-5 ">
+                <ButtonUI type="auth" className="m-3">Register</ButtonUI>
+                <ButtonUI type="auth" className="m-3">Login</ButtonUI>
+                </div>
                 <div className="row mt-5">
                     <div className="col-md-5">
                         <div>
@@ -18,6 +22,24 @@ class AuthScreen extends React.Component {
                             <TextField placeholder="Password" className="mt-2" />
                             <div className="d-flex justify-content-center">
                                 <ButtonUI type="contained" className="mt-4">Login</ButtonUI>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-7">Picture</div>
+                </div>
+                <div className="row mt-5">
+                    <div className="col-md-5">
+                        <div>
+                            <h3>Register</h3>
+                            <p className="mt-4">
+                                You get the best recommendation gadget
+                            </p>
+                            <TextField placeholder="Username" className="mt-5" />
+                            <TextField placeholder="Fullname" className="mt-2" />
+                            <TextField placeholder="Password" className="mt-2" />
+                            <TextField placeholder="Repeat Password" className="mt-2" />
+                            <div className="d-flex justify-content-center">
+                                <ButtonUI type="contained" className="mt-4">Register</ButtonUI>
                             </div>
                         </div>
                     </div>
